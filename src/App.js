6885b63d8e5dcea15541fe42dcf899a82e5bd489
yuvrajsingh1010 <yuvrajsinghero@gmail.com>
2024-04-02 01:04:04 +0530

    Add Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // Import React library
+import { Provider } from 'react-redux'; // Import Provider component from react-redux library
+import store from './store'; // Import Redux store
+import TaskInput from './components/TaskInput'; // Import TaskInput component
+import TaskList from './components/TaskList'; // Import TaskList component
+import '../src/App.css'; // Import CSS file for styling
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}> {/* Wrap the entire app with Provider and pass Redux store */}
+      <div className="App"> {/* Main container for the app */}
+        <h1>Todo List</h1> {/* Heading for the Todo List */}
+        <TaskInput /> {/* TaskInput component for adding new tasks */}
+        <TaskList /> {/* TaskList component for displaying tasks */}
+      </div>
+    </Provider>
   );
 }
 
-export default App;
+export default App; // Export the App component
